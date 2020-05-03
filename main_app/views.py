7daccc81965f35d7bca0942dd6eb6c86ca6185c6
Bobby Coleman <colemankidding@gmail.com>
2020-05-03 10:38:50 -0700

    added finch Class

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 
 
+class Finch:
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+finches = [
+  Finch('Lolo', 'tabby', 'foul little demon', 3),
+  Finch('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+  Finch('Raven', 'black tripod', '3 legged cat', 4)
+]
 
 def home(request):
     return render(request, 'home.html')
@@ -8,3 +20,5 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+def finches_index(request):
+    return render(request, 'finches/index.html', { 'finches' : finches })
