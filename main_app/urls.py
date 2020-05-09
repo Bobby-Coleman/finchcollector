52323dde6678e297ee72b153bf4085b9fc741f19
Bobby Coleman <colemankidding@gmail.com>
2020-05-08 20:36:25 -0700

    show only user's cats and impliment authorization

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,11 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('finches/', views.FinchList.as_view(), name='finches_index'),
+    path('finches/', views.finches_index, name='finches_index'),
     path('finch/<int:pk>', views.FinchDetail.as_view(), name='finches_detail'),
     path('finch/create', views.FinchCreate.as_view(), name='finches_create'),
     path('finches/<int:pk>/update',  views.FinchUpdate.as_view(), name='finches_update'),
     path('finches/<int:pk>/',  views.FinchDelete.as_view(), name='finches_delete'),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
 
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Finch
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 def home(request):
     return render(request, 'home.html')
@@ -9,18 +14,24 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-class FinchList(ListView):
-    model = Finch
+    
+@login_required
+def finches_index(request):
+  finches = Finch.objects.filter(user=request.user)
+  return render(request, 'main_app/finch_list.html', { 'finches': finches })
 
-    def get_queryset(self):
-        return Finch.objects.all()
 
-class FinchDetail(DetailView):
+class FinchDetail(LoginRequiredMixin, DetailView):
     model = Finch
 
 class FinchCreate(CreateView):
     model = Finch
-    fields = '__all__' 
+    fields = ['name', 'breed', 'description', 'age']
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
 
 class FinchUpdate(UpdateView):
     model = Finch
@@ -30,3 +41,24 @@ class FinchUpdate(UpdateView):
 class FinchDelete(DeleteView):
     model = Finch
     success_url = '/finches/'
+
+    
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in via code
+      login(request, user)
+      return redirect('finches_index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
